8b53971970c8be1fbe5450537d50888a5ec8e8f3
Christopher Whitley <aristurtledev@gmail.com>
2023-02-15 17:07:56 -0500

    Docs, yeee

---
+++ .docs/ArisDocs.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using ArisDocs;
+
+
+string path = Path.GetFullPath("../../Artifacts/Debug/Build/MonoGame.Aseprite.dll");
+Assembly asm = Assembly.LoadFrom(path);
+AssemblyDocument.LoadXmlDocumentation(asm);
+
+Type? spriteType = asm.GetType("MonoGame.Aseprite.Sprites.Sprite");
+if (spriteType is null)
+{
+    throw new Exception("Unable to get type from assembly");
+}
+Console.WriteLine(spriteType.GetDocumentation());
