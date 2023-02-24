4394c58a7d9194593c19dbdf11b6a348f79d8206
Christopher Whitley <aristurtledev@gmail.com>
2023-02-24 13:49:02 -0500

    Added Tileset processing to pipeline extension

---
+++ source/MonoGame.Aseprite.Content.Pipeline/Processors/TilesetContentProcessor.cs
@@ -0,0 +1,56 @@
+/* ----------------------------------------------------------------------------
+MIT License
+
+Copyright (c) 2018-2023 Christopher Whitley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---------------------------------------------------------------------------- */
+
+using System.ComponentModel;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using MonoGame.Aseprite.Content.Pipeline.ContentTypes;
+using MonoGame.Aseprite.Content.Processors;
+using MonoGame.Aseprite.RawTypes;
+
+namespace MonoGame.Aseprite.Content.Pipeline.Processors;
+
+[ContentProcessor(DisplayName = "Aseprite Tileset Processor - MonoGame.Aseprite")]
+internal sealed class TilesetContentProcessor : ContentProcessor<ContentImporterResult<AsepriteFile>, ContentProcessorResult<TilesetContent>>
+{
+    [DisplayName("Tileset Name")]
+    public string TilesetName { get; set; } = string.Empty;
+
+    [DisplayName("Generate Mipmaps")]
+    public bool GenerateMipmaps { get; set; } = false;
+
+    public override ContentProcessorResult<TilesetContent> Process(ContentImporterResult<AsepriteFile> content, ContentProcessorContext context)
+    {
+        RawTileset rawTileset = TilesetProcessor.ProcessRaw(content.Data, TilesetName);
+        Texture2DContent texture2DContent = ProcessorHelpers.CreateTextureContent(rawTileset.RawTexture, rawTileset.Name);
+        if (GenerateMipmaps)
+        {
+            texture2DContent.GenerateMipmaps(true);
+        }
+
+        TilesetContent tilesetContent = new(rawTileset, texture2DContent);
+
+        return new(tilesetContent);
+    }
+}
