9a23e248d0c03f2dbfdd115bd087916791cfb207
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-04-03 16:48:16 -0400

    Release/6.3.2 (#120)
    * Bump version to 6.3.2
    
    * Simplify build and use common workflows
    
    * Delete old workflow
    
    * Default branch is main

--- build/Program.cs
+++
@@ -1,7 +0,0 @@
-using Cake.Frosting;
-using MonoGame.Aseprite.Build;
-
-return new CakeHost()
-            .UseWorkingDirectory("../")
-            .UseContext<BuildContext>()
-            .Run(args);
--- build/Tasks/Tasks.cs
+++
@@ -1,8 +0,0 @@
-﻿using Cake.Frosting;
-
-namespace MonoGame.Aseprite.Build;
-
-[TaskName("Default")]
-[IsDependentOn(typeof(BuildTask))]
-[IsDependentOn(typeof(PackTask))]
-public sealed class DefaultTask : FrostingTask<BuildContext> { }
